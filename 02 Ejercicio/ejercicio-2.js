9a2ef355c4f1e89c11814c194c2ecbc7301978fd
nezkh <f.marchant.aravena@gmail.com>
2023-01-02 14:42:49 -0300

    carga ejercicio 2

--- 02 Ejercicio/ejercicio-2.js
+++ 02 Ejercicio/ejercicio-2.js
@@ -2,11 +2,11 @@ const yo = {
     nombre: "Francisco",
     edad: 29,
     desarrollador: true,
-    fecha_nacimiento: Date(1993, 11, 5),
+    fecha_nacimiento: new Date(1993, 11, 5),
     libro:{
         titulo:"El portal de los obeliscos",
         autor:"N. K. Jemisin",
-        fecha_anyo: 2016,
+        fecha_anyo: new Date(2016, 07, 16) 
         url:"https://www.amazon.com/-/es/N-K-Jemisin/dp/8466662677"
     }
 }
