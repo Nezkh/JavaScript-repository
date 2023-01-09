0c3d5340276cf1a0b7d7b2ee4ea6a288ad3654ed
nezkh <f.marchant.aravena@gmail.com>
2023-01-09 01:46:31 -0300

    carga ejercicio 3

---
+++ 03 Estructura de Control/factorial-break.js
@@ -0,0 +1,12 @@
+let i = [0];
+let factorial = [1];
+
+while(i < 10){
+    if(i ===10){
+        break;
+    }
+    i++
+    console.log(i)
+    factorial = factorial *i
+    console.log(factorial)
+}
