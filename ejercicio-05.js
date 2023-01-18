af52c4f4066ae131b8c12f2eaf3b2d7f19763db4
nezkh <f.marchant.aravena@gmail.com>
2023-01-17 21:51:26 -0300

    Ejercicio 5 cargado

---
+++ ejercicio-05.js
@@ -0,0 +1,14 @@
+let alturaCm = 181
+let alturaMt = 1.81
+
+let pesoKg = 90.5
+
+let alturaRe = Math.ceil(alturaMt)
+console.log(alturaRe)
+
+let pesoRe = Math.floor(pesoKg)
+console.log(pesoRe)
+
+let a = Number.MAX_VALUE + 1
+
+console.log(a)
