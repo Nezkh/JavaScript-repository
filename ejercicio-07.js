8cf3fd9f3045e92d92585ff5bee4e720c764727e
nezkh <f.marchant.aravena@gmail.com>
2023-01-23 19:35:38 -0300

    carga ejercicio 7

---
+++ ejercicio-07.js
@@ -0,0 +1,11 @@
+const familia = ["Sandra", "Miguel", "Fabian", "Francisco"]
+const familia_set = new Set(familia)
+console.log(familia_set);
+
+// Añadimos mi nombre:
+familia_set.add("Francisco")
+console.log(familia_set);
+
+// Añadim,os JS:
+familia_set.add("JavaScript")
+console.log(familia_set);
